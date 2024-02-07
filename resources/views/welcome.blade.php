a98207ea8cc357d5252d95f8f3d300d057261e84
Gustavo Barbosa <gustavo.guimaraes09@gmail.com>
2024-02-07 10:12:00 -0300

    welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Meu CRUD
+                    CRUD Lendfy
                 </div>
             </div>
         </div>
